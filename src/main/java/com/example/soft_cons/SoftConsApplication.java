fd6a0e30e474a4f57815580df5bc02a497914d4d
barongobi <barongobirn@gmail.com>
2022-06-12 23:49:48 +0800

    Changing the database to mysql

--- src/main/java/com/example/soft_cons/SoftConsApplication.java
+++ src/main/java/com/example/soft_cons/SoftConsApplication.java
@@ -2,6 +2,9 @@ package com.example.soft_cons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SoftConsApplication {
@@ -10,4 +13,13 @@ public class SoftConsApplication {
 		SpringApplication.run(SoftConsApplication.class, args);
 	}
 
+	@Bean
+   public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "PUT", "DELETE", "POST");
+         }
+      };
+   }
 }
